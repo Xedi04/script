4a3f5061f442829ecbebb43b1652ae52d456fdfc
Xedi04 <rzayevaxdic44@gmail.com>
2023-11-02 10:50:27 +0400

    first

---
+++ script.js
@@ -0,0 +1,39 @@
+//Data1:
+let nailCeki=92;
+
+let nailBoy=1.95;
+
+let nihadCeki=78;
+
+let nihadBoy=1.69;
+
+let nailBKI=nailCeki/(nailBoy*nailBoy);
+
+let nihadBKI=nihadCeki/(nihadBoy*nihadBoy);
+
+console.log(nailBKI);
+
+console.log(nihadBKI);
+
+console.log(Boolean(nihadBKI>nailBKI));
+
+//Data2:
+
+let nailKutle=85;
+
+let nailHun=1.76;
+
+let nihadKutle=95;
+
+let nihadHun=1.88;
+
+let nailBki=nailKutle/(nailHun*nailHun);
+
+let nihadBki=nihadKutle/(nihadHun*nihadHun);
+
+console.log(nailBki);
+
+console.log(nihadBki);
+
+console.log(Boolean(nihadBki>nailBki));
+
